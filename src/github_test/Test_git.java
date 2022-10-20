dedca521443c9c67e953a5ab85d6072ebd51b14c
JaeHyeon Lee <project0858@gmail.com>
2022-10-20 19:58:50 +0900

    222

---
+++ src/github_test/Test_git.java
@@ -0,0 +1,16 @@
+package github_test;
+
+public class Test_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(123456789);
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+		// 협업을 위한 테스트 중입니다.
+		// 과연 어떻게 들어올까요
+	}
+
+}
