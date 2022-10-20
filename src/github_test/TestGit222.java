51436f5f72a4de2fd41ddd3b2660c1c3323e49c2
JaeHyeon Lee <project0858@gmail.com>
2022-10-21 08:30:21 +0900

    where yh commit

--- src/github_test/TestGit222.java
+++ src/github_test/TestGit222.java
@@ -5,6 +5,9 @@ public class TestGit222 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//nn 힘들다.
+		System.out.println(21345236246L);
+		
+		//윤후님이 작성한 내용이 들어오지 않는다 어떻게 해야하난
 	}
 
 }
